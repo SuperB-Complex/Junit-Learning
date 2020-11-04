9e155c4e365695912b1f1688ac244847d24ef6e3
Jinjin Ma <jinjinma@Jinjins-MBP.attlocal.net>
2020-11-04 11:31:24 -0600

    add assertTure, assertFalse, assertThrow

--- HealthyCoderApp/src/test/com/healthycoderapp/BMICalculatorTest.java
+++ HealthyCoderApp/src/test/com/healthycoderapp/BMICalculatorTest.java
@@ -1,4 +1,51 @@
+package com.healthycoderapp;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class BMICalculatorTest {
-  
+
+    @Test
+    void should_ReturnTrue_When_DietRecommended() {
+
+        // given
+        double weight = 77.0;
+        double height = 1.72;
+
+        // when
+        boolean recommended = BMICalculator.isDietRecommended(weight, height);
+
+        // then
+        assertTrue(recommended);
+    }
+
+    @Test
+    void should_ReturnFalse_When_DietRecommended() {
+
+        // given
+        double weight = 10.0;
+        double height = 10.0;
+
+        // when
+        boolean recommended = BMICalculator.isDietRecommended(weight, height);
+
+        // then
+        assertFalse(recommended);
+    }
+
+    @Test
+    void should_ThrowArithmeticException_When_DietRecommended() {
+
+        // given
+        double weight = 10.0;
+        double height = 0.0;
+
+        // when
+        Executable executable = () -> BMICalculator.isDietRecommended(weight, height);
+
+        // then
+        assertThrows(ArithmeticException.class, executable);
+    }
 }
