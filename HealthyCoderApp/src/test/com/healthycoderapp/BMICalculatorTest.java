c6b2d4efdbbf775b47e07869f5d30d90bda4aca4
Jinjin Ma <jinjinma@Jinjins-MBP.attlocal.net>
2020-11-04 12:43:30 -0600

    add @ParameterizedTest

--- HealthyCoderApp/src/test/com/healthycoderapp/BMICalculatorTest.java
+++ HealthyCoderApp/src/test/com/healthycoderapp/BMICalculatorTest.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.Executable;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -31,6 +34,52 @@ class BMICalculatorTest {
         // given
         double weight = 77.0;
         double height = 1.72;
+
+        // when
+        boolean recommended = BMICalculator.isDietRecommended(weight, height);
+
+        // then
+        assertTrue(recommended);
+    }
+
+    @ParameterizedTest
+    @ValueSource(doubles = {89.0, 95.0, 110.0})
+    void should_ReturnTrue_When_DietRecommended(Double coderWeight) {
+
+        // given
+        double weight = coderWeight;
+        double height = 1.72;
+
+        // when
+        boolean recommended = BMICalculator.isDietRecommended(weight, height);
+
+        // then
+        assertTrue(recommended);
+    }
+
+    // the parameter value is mandatory
+    @ParameterizedTest
+    @CsvSource(value = {"89.0, 1.72", "95.0, 1.75", "110.0, 1.78"})
+    void should_ReturnTrue_When_DietRecommended(Double coderWeight, Double coderHeight) {
+
+        // given
+        double weight = coderWeight;
+        double height = coderHeight;
+
+        // when
+        boolean recommended = BMICalculator.isDietRecommended(weight, height);
+
+        // then
+        assertTrue(recommended);
+    }
+
+    @ParameterizedTest(name = "weight = {0}, height = {1}")
+    @CsvSource(value = {"89.0, 1.72", "95.0, 1.75", "110.0, 1.78"})
+    void should_ReturnTrue_When_DietRecommended_Parameterized(Double coderWeight, Double coderHeight) {
+
+        // given
+        double weight = coderWeight;
+        double height = coderHeight;
 
         // when
         boolean recommended = BMICalculator.isDietRecommended(weight, height);
